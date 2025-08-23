0f962e50fb99b669686272186abab86a60e909dc
Karine <karine-caldas@hotmail.com>
2025-08-23 19:21:40 -0300

    Adiciona código Java com exemplo de variáveis

---
+++ aula1/Variaveis.java
@@ -0,0 +1,25 @@
+package aula1;
+
+// Conceitos básicos de Java - Variáveis
+public class Variaveis {
+
+    // Variáveis de números inteiros
+    public static void main(String[] args) {
+       // Declaração de variáveis
+        int numero1 = 18;
+        int numero2 = 7;
+
+        // Processamento de dados
+        int resultado = numero1 + numero2;
+        // Saída de dados
+        System.out.println(resultado);
+
+
+         
+        // Variáveis de tipo texto 
+        String nome = "Karine";
+        String sobrenome = "Caldas";
+
+        System.out.println("Meu nome é: "+nome+" "+sobrenome);
+    }
+}
