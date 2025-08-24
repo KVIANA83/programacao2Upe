9b202499bdf3f37fcbb0910f0551522408b11def
Karine <karine-caldas@hotmail.com>
2025-08-23 22:33:55 -0300

    Atualizando conteúdo da aula 1

---
+++ aula1/SaidaPadrao.java
@@ -0,0 +1,10 @@
+package aula1;
+
+public class SaidaPadrao {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(1.5);
+        System.out.println(true);
+        System.out.println("Minha cadeia de caracteres");
+    }
+}
